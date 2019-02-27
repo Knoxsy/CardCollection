fac0c5f781635c225b0472b2b423a59a48021279
sharonfuller <sharon.fuller717@gmail.com>
2019-02-27 10:45:43 -0700

    placed ] in tag factory and changed home.blade.php browse href from index to browse

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -5,5 +5,5 @@ use Faker\Generator as Faker;
 $factory->define(App\Tag::class, function (Faker $faker) {
 
   $tag = ['RC', 'AS', 'HOF', 'DK', 'COACH'];
-  return ['name' => $tag[array_rand($tag)];
+  return ['name' => $tag[array_rand($tag)]];
 });
