56a7a1294f00bbfe15cc8560dd1334b92d6eb103
sharonfuller <sharon.fuller717@gmail.com>
2019-03-06 20:45:51 -0700

    got set to render on set page

--- resources/views/Resource/Set/item.blade.php
+++ resources/views/Resource/Set/item.blade.php
@@ -1,13 +1,22 @@
-<ul>
 
   <!-- <pre>
     {{$item}}
   </pre> -->
-  <li>
-  {{$item->count}}
-  {{$item->genre}}
+  <h1>
+  {{$item->genre}}:
+</h1>
+  <h2>
   {{$item->year}}
   {{$item->brand}}
-  </li>
+</h2>
 
+<ul>
+
+<li>
+
+<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-1">
+  <input type="checkbox" id="checkbox-1" class="mdl-checkbox__input" checked>
+  <span class="mdl-checkbox__label">{{$item->cards}}</span>
+</label>
+</li>
 </ul>
