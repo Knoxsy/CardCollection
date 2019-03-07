e850dd4ed82b902c7b8e9e0f2c04c70aa9e00918
Jeremy Cornell <jeremy.cornell77@gmail.com>
2019-03-06 17:30:24 -0700

    working on connecting non-user pages

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Set;
 use Illuminate\Http\Request;
 
 class SiteController extends Controller
@@ -18,19 +19,17 @@ class SiteController extends Controller
   public function contact() {
     return view('site.contact');
   }
-  public function browse() {
-    return view('site.browse');
-  }
+  // public function browse() {
+  //   return view('site.browse');
+  // }
   public function browse() {
     $data['sets'] = Set::all();
     $data['categories'] = [];
-
     foreach($data['sets'] as $set){
       if(!in_array($set->genre, $data['categories'])){
         array_push($data['categories'], $set->genre);
       }
     }
-
     return view('site.browse', $data);
   }
 }
