8a9bc026be175e124a1db0df5050f02e6a601f2c
sharonfuller <sharon.fuller717@gmail.com>
2019-03-07 22:04:07 -0700

    merged smf_sort with working sorting for data on browse page

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Set;
 use Illuminate\Http\Request;
 
@@ -10,26 +8,45 @@ class SiteController extends Controller
   public function home() {
     return view('site.home');
   }
+  public function register() {
+    return view('site.register');
+  }
   public function login() {
     return view('site.login');
   }
   public function search() {
     return view('site.search');
   }
-  public function contact() {
-    return view('site.contact');
-  }
-  // public function browse() {
-  //   return view('site.browse');
-  // }
   public function browse() {
     $data['sets'] = Set::all();
     $data['categories'] = [];
+    $data['years'] = [];
+    $data['brands'] = [];
     foreach($data['sets'] as $set){
       if(!in_array($set->genre, $data['categories'])){
         array_push($data['categories'], $set->genre);
       }
+      if(!in_array($set->year, $data['years'])){
+        array_push($data['years'], $set->year);
+      }
+      if(!in_array($set->brand, $data['brands'])){
+        array_push($data['brands'], $set->brand);
+      }
     }
+    sort($data['years']);
+    sort($data['brands']);
     return view('site.browse', $data);
+  }
+  public function collection() {
+    return view('collection/collection');
+  }
+  public function set() {
+    return view('collection/set');
+  }
+  public function card() {
+    return view('collection/card');
+  }
+  public function profile() {
+    return view('profile/profile');
   }
 }
--- resources/views/Site/browse.blade.php
+++ resources/views/Site/browse.blade.php
@@ -4,88 +4,78 @@
 
 @section('content')
 
-  <h3>BROWSE PAGE</h3>
+<h3>BROWSE PAGE</h3>
 
-  <div>
-    @foreach($categories as $category)
-      <button class="category_button" id="{{$category}}">{{$category}}</button>
-    @endforeach
+<div>
+  @foreach($categories as $category)
+  <button class="category_button" id="{{$category}}">{{$category}}</button>
+  @endforeach
+</div>
+
+
+<script>
+
+$('.category_button').click(function(){
+  var current_category = $(this).attr('id');
+
+  //YEAR
+  $('#year_list').show().children('div').each(function(){
+    console.log($(this).attr('class'));
+    if($(this).attr('class') == current_category){
+      $(this).slideDown();
+    }else{
+      $(this).slideUp();
+    }
+  });
+
+  //BRAND
+  $('#brand_list').show().children('div').each(function(){
+    console.log($(this).attr('class'));
+    if($(this).attr('class') == current_category){
+      $(this).slideDown();
+    }else{
+      $(this).slideUp();
+    }
+  });
+})
+</script>
+
+
+<!-- BOOTSTRAP TABS -->
+<div class="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
+  <div class="mdl-tabs__tab-bar">
+    <a href="#starks-panel" class="mdl-tabs__tab is-active">YEAR</a>
+    <a href="#lannisters-panel" class="mdl-tabs__tab">BRAND</a>
+
   </div>
 
-
-  <script>
-
-  //NOTE lines 25-31 need to hide the original call
-
-  // $('#year_list').hide();
-  //
-  // $('#year_list').children('div').each(function(){
-  //   $(this).slideUp();
-  // $(this).next().show();
-  // });
-  //
-  // $('#brand_list').children('div').each(function(){
-  //   $(this).hide();
-  // });
-
-    $('.category_button').click(function(){
-      var current_category = $(this).attr('id');
-
-      //YEAR
-      $('#year_list').show().children('div').each(function(){
-        console.log($(this).attr('class'));
-        if($(this).attr('class') == current_category){
-          $(this).slideDown();
-        }else{
-          $(this).slideUp();
-        }
-      });
-
-      //BRAND
-      $('#brand_list').show().children('div').each(function(){
-        console.log($(this).attr('class'));
-        if($(this).attr('class') == current_category){
-          $(this).slideDown();
-        }else{
-          $(this).slideUp();
-        }
-      });
-    })
-  </script>
-
-  <!-- BOOTSTRAP TABS -->
-
-  <div class="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
-    <div class="mdl-tabs__tab-bar">
-      <a href="#starks-panel" class="mdl-tabs__tab is-active">YEAR</a>
-      <a href="#lannisters-panel" class="mdl-tabs__tab">BRAND</a>
-    </div>
-
-    <div class="mdl-tabs__panel is-active" id="starks-panel">
-      <div id="year_list" class="year" type="hidden" style="display:none">
-        @foreach($sets->sortBy('year')->unique('year')->values()->all() as $set)
-          <div id="{{$set->id}}" class="{{$set->genre}}">
-            <h6>{{$set->year}}</h6>
-            <li>
-              <a href="{{route('set.show', $set->id)}}">  {{$set->year}} {{$set->brand}}</a>
-            </li>
-          </div>
-        @endforeach
+  <div class="mdl-tabs__panel is-active" id="starks-panel">
+    <div id="year_list" class="year" type="hidden" style="display:none">
+      @foreach($years as $year)
+      <h6> {{$year}}</h6>
+      @foreach($sets->where('year', $year) as $set)
+      <div id="{{$set->id}}" class="{{$set->genre}}">
+        <li>
+          <a href="{{route('set.show', $set->id)}}">  {{$set->year}} {{$set->brand}}</a>
+        </li>
       </div>
-    </div>
-
-    <div class="mdl-tabs__panel" id="lannisters-panel">
-      <div id="brand_list" class="brand" style="display:none">
-        @foreach($sets->sortBy('brand')->unique('brand')->values()->all() as $set)
-          <div id="{{$set->id}}" class="{{$set->genre}}">
-            <h6>{{$set->brand}}</h6>
-            <li>
-              <a href="{{route('set.show', $set->id)}}">{{$set->year}} {{$set->brand}}</a>
-            </li>
-          </div>
-        @endforeach
-      </div>
+      @endforeach
+      @endforeach
     </div>
   </div>
-
+  <div class="mdl-tabs__panel" id="lannisters-panel">
+    <div id="brand_list" class="brand" style="display:none">
+      @foreach($brands as $brand)
+      <h6>{{$brand}}</h6>
+      @foreach($sets->where('brand', $brand) as $set)
+      <div id="{{$set->id}}" class="{{$set->genre}}">
+        <li>
+          <a href="{{route('set.show', $set->id)}}">{{$set->year}} {{$set->brand}}</a>
+        </li>
+      </div>
+      @endforeach
+      @endforeach
+    </div>
+  </div>
+</div>
 @endsection
