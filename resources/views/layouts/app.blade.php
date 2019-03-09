492edf9fa00f65dbe2908816e56c4aabbaca2fba
Jamie Guay <Bidman22@Nicoles-MacBook-Air.local>
2019-03-09 14:35:15 -0700

    First stages of authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{"Card Collector"}}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -48,7 +48,7 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
-                        @else
+                        @else -->
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -66,8 +66,8 @@
                                     </form>
                                 </div>
                             </li>
-                        @endguest -->
-                    </ul>
+                        @endguest
+                    <!-- </ul> -->
                 </div>
             </div>
         </nav>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,23 @@
+<!DOCTYPE html>
+<!-- <head> -->
+    <!-- <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> -->
 
+    <!-- CSRF Token -->
+    <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
+
+    <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
+
+    <!-- Scripts -->
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
+
+    <!-- Fonts -->
+    <!-- <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css"> -->
+
+    <!-- Styles -->
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+<!-- </head> -->
 <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
       <header class="mdl-layout__header mdl-color--blue-grey-900" class="head">
         <div class="mdl-layout__header-row mdl-color--blue-grey-900">
@@ -21,7 +40,22 @@
               </a>
 
           @else
-            <p>Welcome back, {{Auth::user()->name}}!</p>
+          <ul>
+            <div>
+              <p style="color: red; font-family: fantasy; font-size: 20px">{{ Auth::user()->name }}
+                  <a href="{{ route('logout') }}"
+                  <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored mdl-color--light-green-900 butts2" onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();">
+                      {{ __('Logout') }}
+                    </button>
+                  </a>
+
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      @csrf
+                  </form>
+              </p>
+            </div>
+          </ul>
           @endguest
 
 
@@ -47,6 +81,6 @@
            <a class="mdl-navigation__link mdl-color-text--white" href="/CardCollection/public/set">Set View</a>
            <a class="mdl-navigation__link mdl-color-text--white" href="/CardCollection/public/card">Card View</a>
            <a class="mdl-navigation__link mdl-color-text--white" href="/CardCollection/public/contact">Contact</a>
-
+           <!-- <a class="mdl-navigation__link mdl-color-text--white" href="/CardCollection/public/login">Login</a> -->
         </nav>
     </div>
