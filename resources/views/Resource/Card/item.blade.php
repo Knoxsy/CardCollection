7f91ff3929b82acaf835f19d8497fb72d14603dc
sharonfuller <sharon.fuller717@gmail.com>
2019-03-09 16:39:03 -0700

    added add to my collection  button in card view

--- resources/views/Resource/Card/item.blade.php
+++ resources/views/Resource/Card/item.blade.php
@@ -3,7 +3,11 @@
 @section('title', 'Cardboard Gems - Set View Page')
 
 @section('content')
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
   <h3>SINGLE CARD VIEW PAGE</h3>
   <img src="{{$card->front_image}}" height="300" width="200" /><br />
   #{{$card->card_number}}&nbsp &nbsp{{$card->name}}
+  <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored mdl-color--light-green-900 butts3">Add to My Collection
+
+  </button>
 @endsection
