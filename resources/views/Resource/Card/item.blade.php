c2be607429d0091603014c4c629346d4f0a2b8e0
Jeremy Cornell <jeremy.cornell77@gmail.com>
2019-03-10 14:35:29 -0600

    saving progress on card page

--- resources/views/Resource/Card/item.blade.php
+++ resources/views/Resource/Card/item.blade.php
@@ -3,8 +3,10 @@
 @section('title', 'Cardboard Gems - Set View Page')
 
 @section('content')
-<link rel="stylesheet" href="{{asset('css/style.css')}}">
-  <h3>SINGLE CARD VIEW PAGE</h3>
+<link rel="stylesheet" href="{{asset('public/css/style.css')}}">
+
+  <h5>Cards from {Set Name}</h5>
+
   <div>
     <a href="{{$card->card_id}}"><img src="{{asset('photos/buttons/previous_button.png')}}" height="75px" /></a>
     <img src="{{$card->front_image}}" height="350" width="250" />
@@ -21,14 +23,17 @@
     </button>
   </div>
   <div>
-    <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+    <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored mdl-color--red-900">
       <i class="material-icons">remove</i>
     </button>
+  </div>
+  <div>
+    <img src="{{asset('photos/buttons/have_star.png')}}" width="60px"/>
   </div>
 
   @else
   <div>
-    <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored mdl-color--green-900" disabled>
+    <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored mdl-color--light-green-900" disabled>
       <i class="material-icons">add</i>
     </button>
   </div>
@@ -37,6 +42,11 @@
       <i class="material-icons">remove</i>
     </button>
   </div>
+  <div>
+    <img src="{{asset('photos/buttons/have_star.png')}}" width="60px"/>
+  </div>
   @endauth
+
+
 
 @endsection
