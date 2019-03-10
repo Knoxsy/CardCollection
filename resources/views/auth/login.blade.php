d0d3509cc9cec37c103f32d83db9010aa34004d7
Jamie Guay <Bidman22@Nicoles-MacBook-Air.local>
2019-03-10 08:07:42 -0600

    before styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,10 @@
 @extends('master/master')
 
 @section('content')
-<div class="container" style="opacity: 1.0;">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+<div class=".demo-card-square.mdl-card" style="opacity: 1.0;">
                 <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
-                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
@@ -23,7 +16,7 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
+                            </div>
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
