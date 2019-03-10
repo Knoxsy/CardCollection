0d081cf40a5ae920237dc471635cb24751615179
Jeremy Cornell <jeremy.cornell77@gmail.com>
2019-03-10 15:17:58 -0600

    saving progress on card page

--- resources/views/Resource/Card/item.blade.php
+++ resources/views/Resource/Card/item.blade.php
@@ -5,25 +5,29 @@
 @section('content')
 <link rel="stylesheet" href="{{asset('public/css/style.css')}}">
 
-  <h5>Cards from {Set Name}</h5>
+  <h5>#{{$card->card_number}}&nbsp{{$card->name}} ~ {Set Name}</h5>
 
   <div>
-    <a href="{{$card->card_id}}"><img src="{{asset('photos/buttons/previous_button.png')}}" height="75px" /></a>
+    <a href="{{$card->card_id}}" id="previous">
+      <img src="{{asset('photos/buttons/previous_button.png')}}"height="75px" />
+    </a>
     <img src="{{$card->front_image}}" height="350" width="250" />
-    <a href="{{$card->card_id}}"><img src="{{asset('photos/buttons/next_button.png')}}"  height="75px" /></a>
+    <a href="{{$card->card_id}}" id='next'>
+      <img src="{{asset('photos/buttons/next_button.png')}}" height="75px" />
+    </a>
   </div>
-  <div>
-    <h6>#{{$card->card_number}}&nbsp &nbsp{{$card->name}}</h6>
-  </div>
+
 
   @auth
   <div>
-    <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored mdl-color--light-green-900">
+    <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored
+    mdl-color--light-green-900" id="add">
       <i class="material-icons">add</i>
     </button>
   </div>
   <div>
-    <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored mdl-color--red-900">
+    <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored
+    mdl-color--red-900" id="remove">
       <i class="material-icons">remove</i>
     </button>
   </div>
@@ -33,20 +37,22 @@
 
   @else
   <div>
-    <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored mdl-color--light-green-900" disabled>
+    <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored
+    mdl-color--light-green-900" id="add" disabled>
       <i class="material-icons">add</i>
     </button>
   </div>
   <div>
-    <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored mdl-color--red-900" disabled>
+    <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored
+    mdl-color--red-900" id="remove" disabled>
       <i class="material-icons">remove</i>
     </button>
   </div>
-  <div>
-    <img src="{{asset('photos/buttons/have_star.png')}}" width="60px"/>
-  </div>
+
   @endauth
 
+  <script>
 
+  </script>
 
 @endsection
