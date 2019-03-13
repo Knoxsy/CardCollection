b774e4bd450c95e714a158a3f14778264bfc0caf
Jeremy Cornell <jeremy.cornell77@gmail.com>
2019-03-12 20:31:55 -0600

    merging jsc_devinto Dev

--- resources/views/Resource/Set/item.blade.php
+++ resources/views/Resource/Set/item.blade.php
@@ -10,27 +10,6 @@
   <span class="setCount">Count: {{$set->count}}</span>
 </div>
 
-<<<<<<< HEAD
-<!-- <table width="350px" border> -->
-    <tr>
-      <table width="550px" align="center">
-      <th class="haveOnSet" width="550px">Have</th>
-      <table class=" mdl-js-data-table mdl-data-table--selectable " width="550px" align="center">
-        <thead>
-      <th width="150px">Card #</th>
-      <th width="350px">Name</th>
-    </tr>
-  </thead>
-
-  <tbody>
-    @foreach($cards as $card)
-    <tr>
-      <!-- <td class=" mdl-js-data-table mdl-data-table--selectable "></td> -->
-      <td style="center"><a href="{{route('card.show', $card->id)}}" >{{$card->card_number}}</a></td>
-      <td><a href="{{route('showcard', $card->id)}}">{{$card->name}}</a></td>
-    </tr>
-=======
-
 <body>
 
   <div class="checklist">
@@ -77,55 +56,5 @@ function openCard(evt, id) {
   evt.currentTarget.className += " active";
 }
 </script>
-
-
-
-
-  <!-- <div class="checklist">
-    <table class="mdl-js-data-table mdl-data-table--selectable" width="550px" align="center">
-      <thead>
-        <th width="150px">Card #</th>
-        <th width="350px">Name</th>
-      </thead>
-      <tbody>
-        @foreach($cards as $card)
-        <tr>
-          <td>
-            <span class="card">{{$card->card_number}}</span>
-          </td>
-          <td>
-            {{$card->name}}
-          </td>
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
-  </div> -->
-
-
-  <div class="showcard">
-    @foreach ($cards as $card)
-      <div>
-        <img src="{{$card->front_image}}" height="350" width="250" />
-      </div>
-      <h5>#{{$card->card_number}}&nbsp{{$card->name}}</h5>
->>>>>>> jsc_dev
-    @endforeach
-  </div>
-
-      <!-- <div>
-        <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored
-        mdl-color--light-green-900" id="add" disabled>
-          <i class="material-icons">add</i>
-        </button>
-      </div>
-
-      <div>
-        <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored
-        mdl-color--red-900" id="remove" disabled>
-        <i class="material-icons">remove</i>
-        </button>
-      </div> -->
-
 
 @endsection
