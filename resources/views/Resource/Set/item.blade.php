3a9496c341dafe9953822eb7fcb86b238620795b
Jeremy Cornell <jeremy.cornell77@gmail.com>
2019-03-12 18:08:44 -0600

    save progress on set page implementing the card view

--- resources/views/Resource/Set/item.blade.php
+++ resources/views/Resource/Set/item.blade.php
@@ -5,32 +5,56 @@
 @section('content')
 <link rel="stylesheet" href="{{asset('css/style.css')}}">
 
-<div>
-  <h4>{{$set->year}}&nbsp{{$set->brand}}&nbsp{{$set->genre}}</h4><br />
+<div class="set_header">
+  <h3>{{$set->year}}&nbsp{{$set->brand}}&nbsp{{$set->genre}}</h3><br />
   <span class="setCount">Count: {{$set->count}}</span>
 </div>
 
-<!-- <table width="350px" border> -->
-    <tr>
-      <table width="550px" align="center">
-      <th class="haveOnSet" width="550px">Have</th>
-      <table class=" mdl-js-data-table mdl-data-table--selectable " width="550px" align="center">
-        <thead>
-      <th width="150px">Card #</th>
-      <th width="350px">Name</th>
-    </tr>
-  </thead>
+  <div class="checklist">
 
-  <tbody>
-    @foreach($cards as $card)
-    <tr>
-      <!-- <td class=" mdl-js-data-table mdl-data-table--selectable "></td> -->
-      <td style="center"><a href="{{route('card.show', $card->id)}}" >{{$card->card_number}}</a></td>
-      <td><a href="{{route('card.show', $card->id)}}">{{$card->name}}</a></td>
-    </tr>
+    <table class="mdl-js-data-table mdl-data-table--selectable" width="550px" align="center">
+      <thead>
+        <th width="150px">Card #</th>
+        <th width="350px">Name</th>
+      </thead>
+      <tbody>
+        @foreach($cards as $card)
+        <tr>
+          <td>
+            <span class="card">{{$card->card_number}}</span>
+          </td>
+          <td>
+            {{$card->name}}
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+
+
+  <div class="showcard">
+    @foreach ($cards as $card)
+      <div>
+        <img src="{{$card->front_image}}" height="350" width="250" />
+      </div>
+      <h5>#{{$card->card_number}}&nbsp{{$card->name}}</h5>
     @endforeach
-  </tbody>
-<!-- </table> -->
-</table>
-</table>
+  </div>
+
+      <!-- <div>
+        <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored
+        mdl-color--light-green-900" id="add" disabled>
+          <i class="material-icons">add</i>
+        </button>
+      </div>
+
+      <div>
+        <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored
+        mdl-color--red-900" id="remove" disabled>
+        <i class="material-icons">remove</i>
+        </button>
+      </div> -->
+
+
 @endsection
