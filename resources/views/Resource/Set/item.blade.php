dda1fd64f2c841b46e73753d51a678581610a3ed
Jeremy Cornell <jeremy.cornell77@gmail.com>
2019-03-12 19:59:44 -0600

    save progress on set page implementing the card view

--- resources/views/Resource/Set/item.blade.php
+++ resources/views/Resource/Set/item.blade.php
@@ -10,8 +10,133 @@
   <span class="setCount">Count: {{$set->count}}</span>
 </div>
 
-  <div class="checklist">
 
+<style>
+* {box-sizing: border-box}
+body {font-family: "Lato", sans-serif;}
+
+/* Style the tab */
+.tab {
+  float: left;
+  border: 1px solid #ccc;
+  background-color: #f1f1f1;
+  width: 30%;
+  height: 300px;
+}
+
+/* Style the buttons inside the tab */
+.tab button {
+  display: block;
+  background-color: inherit;
+  color: black;
+  padding: 22px 16px;
+  width: 100%;
+  border: none;
+  outline: none;
+  text-align: left;
+  cursor: pointer;
+  font-size: 17px;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #ddd;
+}
+
+/* Create an active/current "tab button" class */
+.tab button.active {
+  background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+  float: left;
+  padding: 0px 12px;
+  border: 1px solid #ccc;
+  width: 70%;
+  border-left: none;
+  height: 300px;
+  display: none;
+}
+
+/* Clear floats after the tab */
+.clearfix::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+</style>
+</head>
+<body>
+
+  <div class="checklist">
+    <table class="mdl-js-data-table mdl-data-table--selectable" width="550px" align="center">
+      <thead>
+        <th width="150px">Card #</th>
+        <th width="350px">Name</th>
+      </thead>
+      <tbody>
+        @foreach($cards as $card)
+        <tr>
+          <td class="tablinks" onmouseover="openCity(event, '{{$card->id}}')">
+            {{$card->card_number}}
+          </td>
+          <td class="tablinks" onmouseover="openCity(event, '{{$card->id}}')">
+            {{$card->name}}
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+
+  @foreach ($cards as $card)
+  <div id="{{$card->id}}" class="tabcontent">
+      <img src="{{$card->front_image}}" height="350" width="250" />
+    <h5>#{{$card->card_number}}&nbsp{{$card->name}}</h5>
+  </div>
+  @endforeach
+
+<!-- <div id="London" class="tabcontent">
+  <h3>London</h3>
+  <p>London is the capital city of England.</p>
+</div>
+
+<div id="Paris" class="tabcontent">
+  <h3>Paris</h3>
+  <p>Paris is the capital of France.</p>
+</div>
+
+<div id="Tokyo" class="tabcontent">
+  <h3>Tokyo</h3>
+  <p>Tokyo is the capital of Japan.</p>
+</div> -->
+
+<div class="clearfix"></div>
+
+
+
+
+<script>
+function openCity(evt, id) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(id).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+</script>
+
+
+
+
+  <!-- <div class="checklist">
     <table class="mdl-js-data-table mdl-data-table--selectable" width="550px" align="center">
       <thead>
         <th width="150px">Card #</th>
@@ -30,7 +155,7 @@
         @endforeach
       </tbody>
     </table>
-  </div>
+  </div> -->
 
 
   <div class="showcard">
