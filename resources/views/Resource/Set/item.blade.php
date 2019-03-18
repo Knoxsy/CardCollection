b43703cb64f171b2470c16966c9a48cddcd63951
Jeremy Cornell <jeremy.cornell77@gmail.com>
2019-03-18 11:44:38 -0600

    set page items change when clicked

--- resources/views/Resource/Set/item.blade.php
+++ resources/views/Resource/Set/item.blade.php
@@ -3,69 +3,53 @@
 @section('title', 'Cardboard Gems')
 
 @section('content')
-<link rel="stylesheet" href="{{asset('css/style.css')}}">
 
 <div class="set_header">
   <h3>{{$set->year}}&nbsp{{$set->brand}}&nbsp{{$set->type}}</h3><br />
   <span class="setCount">Count: {{$set->count}}</span>
 </div>
 
-<body>
+<div class="checklist">
+  <table>
+    <thead>
+      <th width="50px">Have<br />
+        <input type="checkbox" id="select_all"/>
+      </th>
+      <th width="150px">Card #</th>
+      <th width="350px">Name</th>
+    </thead>
 
-  <div class="checklist">
-    <table width="550px" align="center">
-      <thead>
+    <tbody>
+      @foreach($cards as $card)
+      <tr>
+        <td class="tablinks" onmouseover="openCard(event, '{{$card->id}}')">
+          <input type="checkbox" class="check checkbox" id="select_all" />
+        </td>
+        <td class="tablinks" onmouseover="openCard(event, '{{$card->id}}')">
+          {{$card->card_number}}
+        </td>
+        <td class="tablinks" onmouseover="openCard(event, '{{$card->id}}')">
+          {{$card->name}}
+        </td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+</div>
 
-        @auth
-        <th width="50px">Have<br />
-          <input type="checkbox" id="select_all"/>
-        </th>
-        @else
-        <th width="50px">Have<br />
-          <input type="checkbox" id="select_all" disabled/>
-        </th>
-        @endauth
-
-        <th width="150px">Card #</th>
-        <th width="350px">Name</th>
-      </thead>
-      <tbody>
-        @foreach($cards as $card)
-        <tr>
-
-          @auth
-          <td class="tablinks" onmouseover="openCard(event, '{{$card->id}}')">
-            <input class="checkbox" type="checkbox" />
-          </td>
-          @else
-          <td class="tablinks" onmouseover="openCard(event, '{{$card->id}}')">
-            <input class="checkbox" type="checkbox" disabled/>
-          </td>
-          @endauth
-
-          <td class="tablinks" onmouseover="openCard(event, '{{$card->id}}')">
-            {{$card->card_number}}
-          </td>
-          <td class="tablinks" onmouseover="openCard(event, '{{$card->id}}')">
-            {{$card->name}}
-          </td>
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
+@foreach ($cards as $card)
+<div id="{{$card->id}}" class="tabcontent">
+  <div class="card_container">
+    <img src="{{$card->front_image}}" height="350" width="250" />  
   </div>
-
-  @foreach ($cards as $card)
-  <div id="{{$card->id}}" class="tabcontent">
-      <img src="{{$card->front_image}}" height="350" width="250" />
-    <h5>#{{$card->card_number}}&nbsp{{$card->name}}</h5>
-  </div>
-  @endforeach
-  <div class="clearfix"></div>
-
+  <h5>#{{$card->card_number}}&nbsp{{$card->name}}</h5>
+</div>
+@endforeach
+<div class="clearfix"></div>
 
 <script>
-  //Hover - Show Card Function
+
+  //HOVER - SHOW CARD FUNCTION
   function openCard(evt, id) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -80,45 +64,44 @@
     evt.currentTarget.className += " active";
   }
 
-  // Select all checkboxes function
+  // SELECT ALL CHECKBOXES FUNCTION
   var select_all = document.getElementById("select_all"); //select all checkbox
   var checkboxes = document.getElementsByClassName("checkbox"); //checkbox items
 
   select_all.addEventListener("change", function(e){
-	  for (i = 0; i < checkboxes.length; i++) {
-		  checkboxes[i].checked = select_all.checked;
-	  }
+    for (i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].checked = select_all.checked;
+    }
   });
 
   for (var i = 0; i < checkboxes.length; i++) {
-	  checkboxes[i].addEventListener('change', function(e){ //".checkbox" change
-		  //uncheck "select all", if one of the listed checkbox item is unchecked
-		  if(this.checked == false){
-			  select_all.checked = false;
-		  }
-		  //check "select all" if all checkbox items are checked
-		  if(document.querySelectorAll('.checkbox:checked').length == checkboxes.length){
-			  select_all.checked = true;
-		   }
-	  });
+    checkboxes[i].addEventListener('change', function(e){ //".checkbox" change
+      //uncheck "select all", if one of the listed checkbox item is unchecked
+      if(this.checked == false){
+        select_all.checked = false;
+      }
+      //check "select all" if all checkbox items are checked
+      if(document.querySelectorAll('.checkbox:checked').length == checkboxes.length){
+        select_all.checked = true;
+      }
+    });
   }
 
-  // //Change TableRow when checkbox is clicked
-  // $(document).ready(function() {
-  //   $('tr').click(function() {
-  //     var inp = $(this).find('.check');
-  //     var tr = $(this).closest('tr');
-  //     inp.prop('checked', !inp.is(':checked'))
-  //
-  //     tr.toggleClass('isChecked', inp.is(':checked'));
-  //   });
-  //
-  //   // do nothing when clicking on checkbox, but bubble up to tr
-  //   $('.check').click(function(e) {
-  //     e.preventDefault();
-  //   });
-  // });
+  // ROW CHANGE ON CLICK FUNCTION
+  $(document).ready(function() {
+    $('tr').click(function() {
+      var inp = $(this).find('.check');
+      var tr = $(this).closest('tr');
+      inp.prop('checked', !inp.is(':checked'))
 
+      tr.toggleClass('isChecked', inp.is(':checked'));
+    });
+
+    // do nothing when clicking on checkbox, but bubble up to tr
+    $('.check').click(function(e) {
+      e.preventDefault();
+    });
+  });
 </script>
 
 @endsection
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,60 +3,56 @@
 @section('content')
 <link rel="stylesheet" href="{{asset('css/style.css')}}">
   <div class="demo-card-square2 mdl-card mdl-shadow--2dp">
-                <div class="LoginFont">{{ __('LOGIN') }}</div>
-                <br>
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                            <label for="email" class="SecondType">{{ __('E-Mail Address') }}</label>
-                            <div>
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" size ="35" value="{{ old('email') }}" required autofocus>
+    <div class="LoginFont">{{ __('LOGIN') }}</div>
+    <br>
+    <form method="POST" action="{{ route('login') }}">
+      @csrf
+      <label for="email" class="SecondType">{{ __('E-Mail Address') }}</label>
+      <div>
+        <input id="email" type="email" class="form-control{{ $errors->has('email')
+        ? ' is-invalid'
+        : '' }}" name="email" size ="35" value="{{ old('email') }}"
+        required autofocus>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+        @if ($errors->has('email'))
+          <span class="invalid-feedback" role="alert">
+            <strong>{{ $errors->first('email') }}</strong>
+          </span>
+        @endif
 
-                        <div>
-                            <br>
-                            <label for="password" class="SecondType">{{ __('Password') }}</label>
+        <div>
+          <br>
+          <label for="password" class="SecondType">{{ __('Password') }}</label>
+          <div>
+            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" size ="35" required>
 
-                            <div>
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" size ="35" required>
+            @if ($errors->has('password'))
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('password') }}</strong>
+              </span>
+            @endif
+          </div>
+        </div>
 
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                                <div>
-                                    <br>
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+        <div>
+          <br>
+          <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+          <label class="form-check-label" for="remember">
+            {{ __('Remember Me') }}
+          </label>
+        </div>
+        <br>
+        <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored mdl-color--light-green-900 butts2">
+          {{ __('Login') }}
+        </button>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                                <br>
-                                <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored mdl-color--light-green-900 butts2">
-                                    {{ __('Login') }}
-                                </button>
-                                @if (Route::has('password.request'))
-                                    <a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored mdl-color--light-green-900 butts2" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-
-
-                    </form>
-                </div>
-            </div>
-
-
-
-
-
+        @if (Route::has('password.request'))
+          <a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored mdl-color--light-green-900 butts2" href="{{ route('password.request') }}">
+            {{ __('Forgot Your Password?') }}
+          </a>
+        @endif
+      </div>
+    </form>
+  </div>
 
 @endsection
