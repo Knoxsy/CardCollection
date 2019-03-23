2b6ea7a278179892a3dcdfaaf660816d291ca6f6
sharonfuller <sharon.fuller717@gmail.com>
2019-03-23 16:41:48 -0600

    got the profile page to render my cards with number of cards and sets

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -53,20 +53,41 @@ class SiteController extends Controller
 
       // QUERY FOR RETRIEVING USER CARDS!
       //SELECT * FROM cards INNER JOIN my_cards ON cards.id = my_cards.card_id WHERE my_cards.user_id = 1;
-      $data['cards'] = DB::table('cards')->join('my_cards', 'cards.id', '=', 'my_cards.card_id')->where('my_cards.user_id','=',$user->id)->get();
+      $data['cards'] = DB::table('cards')->join('my_cards', 'cards.id', '=', 'my_cards.card_id')->join('sets', 'cards.set_id', '=', 'sets.id')->where('my_cards.user_id','=',$user->id)->get();
 
-      $data['sets'] = DB::table(
-        'mycards')->join('cards', 'my_cards.card_id', '=', 'cards.id')->join('sets', 'sets.id', '=', 'cards.set_id')->where('my_cards.user_id', '=', $user->id)->get();
+      // QUERY FOR RETRIEVING USER SETS
+      // SELECT * FROM sets INNER JOIN cards ON sets.id = cards.set_id INNER JOIN my_cards ON my_cards.card_id = cards.id WHERE my_cards.user_id = 1;
+      $data['sets'] = DB::table('sets')->join('cards', 'sets.id', '=', 'cards.set_id')->join('my_cards', 'my_cards.card_id', '=', 'cards.id')->where('my_cards.user_id', '=', $user->id)->get();
 
-      // SELECT * FROM my_cards INNER JOIN cards ON my_cards.card_id = cards.id INNER JOIN sets ON sets.id = cards.set_id WHERE my_cards.user_id = 1;
 
-      $data['sets'] = array();
-      foreach ($data['cards'] as $card){
+      // $data['categories'] = [];
+      // $data['years'] = [];
+      // $data['brands'] = [];
+      //
+      // foreach($data['sets'] as $set){
+      //   if(!in_array($set->genre, $data['categories'])){
+      //     array_push($data['categories'], $set->genre);
+      //   }
+      //   if(!in_array($set->year, $data['years'])){
+      //     array_push($data['years'], $set->year);
+      //   }
+      //   if(!in_array($set->brand, $data['brands'])){
+      //     array_push($data['brands'], $set->brand);
+      //    }
+      // }
+      // sort($data['years']);
+      // sort($data['brands']);
+
+
+
+
+      // $data['sets'] = array();
+      // foreach ($data['cards'] as $card){
         // $set = $card->set();
         // if(!in_array($card, $data['cards'])){
           // array_push($data['sets'], $mycard->card_id);
         // }
-      }
+      //}
       return view('profile', $data);
     } else {
       return redirect('login')->with( 'auth','Unauthorized Page! You Must login to view collections.');
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -15,8 +15,10 @@
 
 <pre>
 mycards count: {{$mycards->count()}}
-sets count: {{count($sets)}}
 
+
+<!-- @foreach($sets as $set)
+@endforeach -->
 
 @foreach($mycards as $mycard)
   mycard: {{$mycard->condition}}
@@ -25,9 +27,15 @@ sets count: {{count($sets)}}
   CARD: {{$card->name}}
 @endforeach
 
+@foreach($sets->where('genre', 'Basketball') as $basketball)
+  SET: {{$basketball->set_id}}
+      {{$basketball->genre}}
+      {{$basketball->brand}}
+      {{$basketball->year}}
+      {{$basketball->type}}
+@endforeach
 
-</pre>
-
+Basketball count: {{$sets->where('genre','Basketball')->count()}}
 
   <div class="collection_stats">
     <table>
@@ -36,40 +44,41 @@ sets count: {{count($sets)}}
         <th>Cards</th>
         <th>Comp. Sets</th>
       </thead>
+
       <tr>
-        <td class="tablinks" onmouseover="openCard(event, 'baseball')">Baseball Cards</td>
-        <td class="tablinks" onmouseover="openCard(event, 'baseball')"></td>
-        <td class="tablinks" onmouseover="openCard(event, 'baseball')"></td>
+        <td class="tablinks" id="Baseball" onmouseover="openCard(event, 'baseball')">Baseball Cards</td>
+        <td class="tablinks" id="Baseball" onmouseover="openCard(event, 'baseball')">{{$cards->where('genre', 'Baseball')->count()}}</td>
+        <td class="tablinks" id="Baseball" onmouseover="openCard(event, 'baseball')">{{$sets->where('genre','Baseball')->count()}}</td>
       </tr>
       <tr>
-        <td class="tablinks" onmouseover="openCard(event, 'basketball')">Basketball Cards</td>
-        <td class="tablinks" onmouseover="openCard(event, 'basketball')"></td>
-        <td class="tablinks" onmouseover="openCard(event, 'basketball')"></td>
+        <td class="tablinks" id="Basketball" onmouseover="openCard(event, 'basketball')">Basketball Cards</td>
+        <td class="tablinks" id="Basketball" onmouseover="openCard(event, 'basketball')">{{$cards->where('genre', 'Basketball')->count()}}</td>
+        <td class="tablinks" id="Basketball" onmouseover="openCard(event, 'basketball')">{{$sets->where('genre', 'Basketball')->count()}}</td>
       </tr>
       <tr>
-        <td class="tablinks" onmouseover="openCard(event, 'football')">Football Cards</td>
-        <td class="tablinks" onmouseover="openCard(event, 'football')"></td>
-        <td class="tablinks" onmouseover="openCard(event, 'football')"></td>
+        <td class="tablinks" id="Football" onmouseover="openCard(event, 'football')">Football Cards</td>
+        <td class="tablinks" id="Football" onmouseover="openCard(event, 'football')">{{$cards->where('genre', 'Football')->count()}}</td>
+        <td class="tablinks" id="Football" onmouseover="openCard(event, 'football')">{{$sets->where('genre', 'Football')->count()}}</td>
       </tr>
       <tr>
-        <td class="tablinks" onmouseover="openCard(event, 'miscsports')">Misc. Sports Cards</td>
-        <td class="tablinks" onmouseover="openCard(event, 'miscsports')"></td>
-        <td class="tablinks" onmouseover="openCard(event, 'miscsports')"></td>
+        <td class="tablinks" id="MiscSports" onmouseover="openCard(event, 'miscsports')">Misc. Sports Cards</td>
+        <td class="tablinks" id="MiscSports" onmouseover="openCard(event, 'miscsports')">{{$cards->where('genre', 'Misc')->count()}}</td>
+        <td class="tablinks" id="MiscSports" onmouseover="openCard(event, 'miscsports')">{{$sets->where('genre', 'Misc')->count()}}</td>
       </tr>
       <tr>
-        <td class="tablinks" onmouseover="openCard(event, 'nonsports')">Non-Sports Cards</td>
-        <td class="tablinks" onmouseover="openCard(event, 'nonsports')"></td>
-        <td class="tablinks" onmouseover="openCard(event, 'nonsports')"></td>
+        <td class="tablinks" id="Nonsports" onmouseover="openCard(event, 'nonsports')">Non-Sports Cards</td>
+        <td class="tablinks" id="Nonsports" onmouseover="openCard(event, 'nonsports')">{{$cards->where('genre', 'Non-Sports')->count()}}</td>
+        <td class="tablinks" id="Nonsports" onmouseover="openCard(event, 'nonsports')">{{$sets->where('genre', 'Non-Sports')->count()}}</td>
       </tr>
       <tr>
-        <td class="tablinks" onmouseover="openCard(event, 'gaming')">Gaming Cards</td>
-        <td class="tablinks" onmouseover="openCard(event, 'gaming')"></td>
-        <td class="tablinks" onmouseover="openCard(event, 'gaming')"></td>
+        <td class="tablinks" id="Gaming" onmouseover="openCard(event, 'gaming')">Gaming Cards</td>
+        <td class="tablinks" id="Gaming" onmouseover="openCard(event, 'gaming')">{{$cards->where('genre', 'Gaming')->count()}}</td>
+        <td class="tablinks" id="Gaming" onmouseover="openCard(event, 'gaming')">{{$sets->where('genre', 'Gaming')->count()}}</td>
       </tr>
       <tr>
         <td>Total Collection</td>
-        <td></td>
-        <td></td>
+        <td>{{$mycards->count()}}</td>
+        <td>{{$sets->count()}}</td>
       </tr>
     </table>
   </div>
@@ -77,6 +86,10 @@ sets count: {{count($sets)}}
   <div class="showsets" id="baseball">
     <h5>Baseball Sets</h5>
     <hr />
+    @foreach($sets->where('genre','Baseball') as $set)
+    {{$set->year}} {{$set->brand}}
+    @endforeach
+
     Hover over the Genre and get the lists of sets to show in this div
   </div>
   <div class="showsets" id="basketball">
@@ -87,32 +100,38 @@ sets count: {{count($sets)}}
   <div class="showsets" id="football">
     <h5>Football Sets</h5>
     <hr />
+    @foreach($sets->where('genre','Basketball') as $set)
+    {{$set->year}} {{$set->brand}}
+    @endforeach
     Hover over the Genre and get the lists of sets to show in this div
   </div>
   <div class="showsets" id="miscsports">
     <h5>Misc. Sports Sets</h5>
     <hr />
+    @foreach($sets->where('genre','Misc') as $set)
+    {{$set->year}} {{$set->brand}}
+    @endforeach
     Hover over the Genre and get the lists of sets to show in this div
   </div>
   <div class="showsets" id="nonsports">
     <h5>Non-Sports Sets</h5>
     <hr />
+    @foreach($sets->where('genre','Non-Sports') as $set)
+    {{$set->year}} {{$set->brand}}
+    @endforeach
     Hover over the Genre and get the lists of sets to show in this div
   </div>
   <div class="showsets" id="gaming">
     <h5>Gaming Sets</h5>
     <hr />
+    @foreach($sets->where('genre','Gaming') as $set)
+    {{$set->year}} {{$set->brand}}
+    @endforeach
     Hover over the Genre and get the lists of sets to show in this div
   </div>
 
   <div class="clearfix"></div>
-  @foreach($sets as $set)
-  <div id={"{$set->id}}"  class="{{$set}}">
-    <h1>HIIIIIIII</h1>
-    {{$set->count()}}
-    console.log({{$set}})
-  </div>
-  @endforeach
+
 
 <script>
 function openCard(evt, id) {
