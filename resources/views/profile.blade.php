56b70107c601956cd1426ebbfa50861b70cde214
sharonfuller <sharon.fuller717@gmail.com>
2019-03-23 17:58:20 -0600

    got the sets to link sortof but not all

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,139 +1,151 @@
 @extends('master/master')
 
+@section('title', 'Profile Page')
+
 @section('content')
 
 <link rel="stylesheet" href="{{asset('css/style.css')}}">
-  <div>
-    <h5>{{ Auth::user()->name }}, welcome to your collection!</h5>
-      <!-- @if($errors->any())
-      <h4>{{$errors->first()}}</h4>
-      @endif -->
+<div>
+  <h5>{{ Auth::user()->name }}, welcome to your collection!</h5>
+  <!-- @if($errors->any())
+  <h4>{{$errors->first()}}</h4>
+  @endif -->
 
+</div>
+
+<div class="collection_stats">
+  <table>
+    <thead>
+      <th>Genre</th>
+      <th>Cards</th>
+      <th>Comp. Sets</th>
+    </thead>
+
+    <tr>
+      <td class="tablinks" id="Baseball" onmouseover="openCard(event, 'baseball')">Baseball Cards</td>
+      <td class="tablinks" id="Baseball" onmouseover="openCard(event, 'baseball')">{{$cards->where('genre', 'Baseball')->count()}}</td>
+      <td class="tablinks" id="Baseball" onmouseover="openCard(event, 'baseball')">{{$sets->where('genre','Baseball')->count()}}</td>
+    </tr>
+    <tr>
+      <td class="tablinks" id="Basketball" onmouseover="openCard(event, 'basketball')">Basketball Cards</td>
+      <td class="tablinks" id="Basketball" onmouseover="openCard(event, 'basketball')">{{$cards->where('genre', 'Basketball')->count()}}</td>
+      <td class="tablinks" id="Basketball" onmouseover="openCard(event, 'basketball')">{{$sets->where('genre', 'Basketball')->count()}}</td>
+    </tr>
+    <tr>
+      <td class="tablinks" id="Football" onmouseover="openCard(event, 'football')">Football Cards</td>
+      <td class="tablinks" id="Football" onmouseover="openCard(event, 'football')">{{$cards->where('genre', 'Football')->count()}}</td>
+      <td class="tablinks" id="Football" onmouseover="openCard(event, 'football')">{{$sets->where('genre', 'Football')->count()}}</td>
+    </tr>
+    <tr>
+      <td class="tablinks" id="MiscSports" onmouseover="openCard(event, 'miscsports')">Misc. Sports Cards</td>
+      <td class="tablinks" id="MiscSports" onmouseover="openCard(event, 'miscsports')">{{$cards->where('genre', 'Misc')->count()}}</td>
+      <td class="tablinks" id="MiscSports" onmouseover="openCard(event, 'miscsports')">{{$sets->where('genre', 'Misc')->count()}}</td>
+    </tr>
+    <tr>
+      <td class="tablinks" id="Nonsports" onmouseover="openCard(event, 'nonsports')">Non-Sports Cards</td>
+      <td class="tablinks" id="Nonsports" onmouseover="openCard(event, 'nonsports')">{{$cards->where('genre', 'Non-Sports')->count()}}</td>
+      <td class="tablinks" id="Nonsports" onmouseover="openCard(event, 'nonsports')">{{$sets->where('genre', 'Non-Sports')->count()}}</td>
+    </tr>
+    <tr>
+      <td class="tablinks" id="Gaming" onmouseover="openCard(event, 'gaming')">Gaming Cards</td>
+      <td class="tablinks" id="Gaming" onmouseover="openCard(event, 'gaming')">{{$cards->where('genre', 'Gaming')->count()}}</td>
+      <td class="tablinks" id="Gaming" onmouseover="openCard(event, 'gaming')">{{$sets->where('genre', 'Gaming')->count()}}</td>
+    </tr>
+    <tr>
+      <td>Total Collection</td>
+      <td>{{$mycards->count()}}</td>
+      <td>{{$sets->count()}}</td>
+    </tr>
+  </table>
+</div>
+
+<div class="showsets" id="baseball">
+  <h5>Baseball Sets</h5>
+  <hr />
+  @foreach($sets->where('genre','Baseball') as $set)
+  <div id="{{$set->id}}" class="{{$set->genre}}">
+    <li>
+      <a href="{{route('showset', $set->id)}}">
+        {{$set->year}} {{$set->brand}}
+      </a>
+    </li>
   </div>
-<h1>HELLO</h1>
+  @endforeach
+</div>
 
-
-<pre>
-mycards count: {{$mycards->count()}}
-
-
-<!-- @foreach($sets as $set)
-@endforeach -->
-
-@foreach($mycards as $mycard)
-  mycard: {{$mycard->condition}}
-@endforeach
-@foreach($cards as $card)
-  CARD: {{$card->name}}
-@endforeach
-
-@foreach($sets->where('genre', 'Basketball') as $basketball)
-  SET: {{$basketball->set_id}}
-      {{$basketball->genre}}
-      {{$basketball->brand}}
-      {{$basketball->year}}
-      {{$basketball->type}}
-@endforeach
-
-Basketball count: {{$sets->where('genre','Basketball')->count()}}
-
-  <div class="collection_stats">
-    <table>
-      <thead>
-        <th>Genre</th>
-        <th>Cards</th>
-        <th>Comp. Sets</th>
-      </thead>
-
-      <tr>
-        <td class="tablinks" id="Baseball" onmouseover="openCard(event, 'baseball')">Baseball Cards</td>
-        <td class="tablinks" id="Baseball" onmouseover="openCard(event, 'baseball')">{{$cards->where('genre', 'Baseball')->count()}}</td>
-        <td class="tablinks" id="Baseball" onmouseover="openCard(event, 'baseball')">{{$sets->where('genre','Baseball')->count()}}</td>
-      </tr>
-      <tr>
-        <td class="tablinks" id="Basketball" onmouseover="openCard(event, 'basketball')">Basketball Cards</td>
-        <td class="tablinks" id="Basketball" onmouseover="openCard(event, 'basketball')">{{$cards->where('genre', 'Basketball')->count()}}</td>
-        <td class="tablinks" id="Basketball" onmouseover="openCard(event, 'basketball')">{{$sets->where('genre', 'Basketball')->count()}}</td>
-      </tr>
-      <tr>
-        <td class="tablinks" id="Football" onmouseover="openCard(event, 'football')">Football Cards</td>
-        <td class="tablinks" id="Football" onmouseover="openCard(event, 'football')">{{$cards->where('genre', 'Football')->count()}}</td>
-        <td class="tablinks" id="Football" onmouseover="openCard(event, 'football')">{{$sets->where('genre', 'Football')->count()}}</td>
-      </tr>
-      <tr>
-        <td class="tablinks" id="MiscSports" onmouseover="openCard(event, 'miscsports')">Misc. Sports Cards</td>
-        <td class="tablinks" id="MiscSports" onmouseover="openCard(event, 'miscsports')">{{$cards->where('genre', 'Misc')->count()}}</td>
-        <td class="tablinks" id="MiscSports" onmouseover="openCard(event, 'miscsports')">{{$sets->where('genre', 'Misc')->count()}}</td>
-      </tr>
-      <tr>
-        <td class="tablinks" id="Nonsports" onmouseover="openCard(event, 'nonsports')">Non-Sports Cards</td>
-        <td class="tablinks" id="Nonsports" onmouseover="openCard(event, 'nonsports')">{{$cards->where('genre', 'Non-Sports')->count()}}</td>
-        <td class="tablinks" id="Nonsports" onmouseover="openCard(event, 'nonsports')">{{$sets->where('genre', 'Non-Sports')->count()}}</td>
-      </tr>
-      <tr>
-        <td class="tablinks" id="Gaming" onmouseover="openCard(event, 'gaming')">Gaming Cards</td>
-        <td class="tablinks" id="Gaming" onmouseover="openCard(event, 'gaming')">{{$cards->where('genre', 'Gaming')->count()}}</td>
-        <td class="tablinks" id="Gaming" onmouseover="openCard(event, 'gaming')">{{$sets->where('genre', 'Gaming')->count()}}</td>
-      </tr>
-      <tr>
-        <td>Total Collection</td>
-        <td>{{$mycards->count()}}</td>
-        <td>{{$sets->count()}}</td>
-      </tr>
-    </table>
+<div class="showsets" id="basketball">
+  <hr />
+  <h5>Basketball Sets</h5>
+  @foreach($sets->where('genre','Basketball') as $set)
+  <div id="{{$set->id}}" class="{{$set->genre}}">
+    <li>
+      <a href="{{route('showset', $set->id)}}">
+        {{$set->year}} {{$set->brand}}
+      </a>
+    </li>
   </div>
+  @endforeach
+</div>
 
-  <div class="showsets" id="baseball">
-    <h5>Baseball Sets</h5>
-    <hr />
-      @foreach($sets->where('genre','Baseball') as $set)
-      <a href = "{{route('showset', $set->id)}}">
-    {{$set->year}} {{$set->brand}}</a>
-    @endforeach
-
+<div class="showsets" id="football">
+  <h5>Football Sets</h5>
+  <hr />
+  @foreach($sets->where('genre','Basketball') as $set)
+  <div id="{{$set->id}}" class="{{$set->genre}}">
+    <li>
+      <a href="{{route('showset', $set->id)}}">
+        {{$set->year}} {{$set->brand}}
+      </a>
+    </li>
   </div>
-  <div class="showsets" id="basketball">
-    <h5>Basketball Sets</h5>
-    @foreach($sets->where('genre','Basketball') as $set)
-    {{$set->year}} {{$set->brand}}
-    @endforeach
-    <hr />
+  @endforeach
+</div>
 
+<div class="showsets" id="miscsports">
+  <h5>Misc. Sports Sets</h5>
+  <hr />
+  @foreach($sets->where('genre','Misc') as $set)
+  <div id="{{$set->id}}" class="{{$set->genre}}">
+    <li>
+      <a href="{{route('showset', $set->id)}}">
+        {{$set->year}} {{$set->brand}}
+      </a>
+    </li>
   </div>
-  <div class="showsets" id="football">
-    <h5>Football Sets</h5>
-    <hr />
-    @foreach($sets->where('genre','Basketball') as $set)
-    {{$set->year}} {{$set->brand}}
-    @endforeach
+  @endforeach
+</div>
 
+<div class="showsets" id="nonsports">
+  <h5>Non-Sports Sets</h5>
+  <hr />
+  @foreach($sets->where('genre','Non-Sports') as $set)
+  <div id="{{$set->id}}" class="{{$set->genre}}">
+    <li>
+      <a href="{{route('showset', $set->id)}}">
+        {{$set->year}} {{$set->brand}}
+      </a>
+    </li>
   </div>
-  <div class="showsets" id="miscsports">
-    <h5>Misc. Sports Sets</h5>
-    <hr />
-    @foreach($sets->where('genre','Misc') as $set)
-    {{$set->year}} {{$set->brand}}
-    @endforeach
+  @endforeach
+</div>
 
+<div class="showsets" id="gaming">
+  <h5>Gaming Sets</h5>
+  <hr />
+  @foreach($sets->where('genre','Gaming') as $set)
+  <div id="{{$set->id}}" class="{{$set->genre}}">
+    <li>
+      <a href="{{route('showset', $set->id)}}">
+        {{$set->year}} {{$set->brand}}
+      </a>
+    </li>
   </div>
-  <div class="showsets" id="nonsports">
-    <h5>Non-Sports Sets</h5>
-    <hr />
-    @foreach($sets->where('genre','Non-Sports') as $set)
-    {{$set->year}} {{$set->brand}}
-    @endforeach
+  @endforeach
+</div>
 
-  </div>
-  <div class="showsets" id="gaming">
-    <h5>Gaming Sets</h5>
-    <hr />
-    @foreach($sets->where('genre','Gaming') as $set)
-    {{$set->year}} {{$set->brand}}
-    @endforeach
+</div>
 
-  </div>
-
-  <div class="clearfix"></div>
+<div class="clearfix"></div>
 
 
 <script>
@@ -153,15 +165,15 @@ function openCard(evt, id) {
 
 </script>
 <!--
-  <div>
-    @if (session('status'))
-      <div class="alert alert-success" role="alert">
-        {{ session('status') }}
-      </div>
-    @endif
+<div>
+@if (session('status'))
+<div class="alert alert-success" role="alert">
+{{ session('status') }}
+</div>
+@endif
 
 
-  </div> -->
+</div> -->
 
 
 @endsection
